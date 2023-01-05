3bc56ed46a95d4707a7a72d5e615995838c5ba03
XRow08 <murilloaugustoe081@gmail.com>
2023-01-05 20:18:41 -0300

    fax

--- src/script.js
+++ src/script.js
@@ -1,34 +1,54 @@
-const contato = document.getElementById('contato')
-const dialog = document.getElementById('dialog')
-const close = document.getElementById('enviar')
+const contato = document.getElementById("contato");
+const dialog = document.getElementById("dialog");
+const close = document.getElementById("enviar");
+const btn_menu = document.getElementById("menu-btn");
+const img_menu = document.getElementById("img-btn");
+const menus = document.getElementsByTagName("a");
 
-contato.addEventListener('click', () => {
-    dialog.style.display = 'flex'
-})
+function menu() {
+  if (btn_menu.classList) btn_menu.classList.add("active");
+  if (img_menu.classList) img_menu.classList.add("active");
+}
 
-close.addEventListener('click', () => {
-    dialog.style.display = 'none'
-})
+function close_menu() {
+  btn_menu.classList.remove("active");
+  img_menu.classList.remove("active");
+}
 
-$(document).ready(function(){
-    $('.carousel').owlCarousel({
-        loop:true,
-        autoplayTimeOut:2000,
-        autoplayHoverPauser:true,
-        responsive:{
-            0:{
-                items:1,
-                nav:false
-            },
-            600:{
-                items:3,
-                nav:false
-            },
-            1000:{
-                items:5,
-                nav:false
-            }
-        }
-    });
+contato.addEventListener("click", () => {
+  dialog.style.display = "flex";
 });
 
+close.addEventListener("click", () => {
+  dialog.style.display = "none";
+});
+
+document.onkeydown = function (e) {
+  if (e.key === "Escape") {
+    dialog.style.display = "none";
+    btn_menu.classList.remove("active");
+    img_menu.classList.remove("active");
+  }
+};
+
+$(document).ready(function () {
+  $(".carousel").owlCarousel({
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPauser: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 3,
+        nav: false,
+      },
+      1000: {
+        items: 5,
+        nav: false,
+      },
+    },
+  });
+});
