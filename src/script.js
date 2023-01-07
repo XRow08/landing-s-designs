59312af5d158b7229057b7706be4c27ce9014f46
XRow08 <murilloaugustoe081@gmail.com>
2023-01-07 19:00:34 -0300

    fix

--- src/script.js
+++ src/script.js
@@ -1,38 +1,50 @@
-const contato = document.getElementById("contato");
-const dialog = document.getElementById("dialog");
-const close = document.getElementById("enviar");
-const btn_menu = document.getElementById("menu-btn");
-const img_menu = document.getElementById("img-btn");
-const menus = document.getElementsByTagName("a");
+const contato = document.getElementById('contato');
+const dialog = document.getElementById('dialog');
+const close = document.getElementById('enviar');
+const btn_menu = document.getElementById('menu-btn');
+const img_menu = document.getElementById('img-btn');
+const menus = document.getElementsByTagName('a');
+const main = document.getElementById('main');
+const main_main = document.getElementById('main_main');
+const fechar = document.getElementById('fechar');
+
+const change = (src) => {
+  main.src = src;
+  main_main.style.display = 'flex';
+};
 
 function menu() {
-  if (btn_menu.classList) btn_menu.classList.add("active");
-  if (img_menu.classList) img_menu.classList.add("active");
+  if (btn_menu.classList.contains('active')) {
+    img_menu.classList.remove('active');
+    btn_menu.classList.remove('active');
+  } else {
+    btn_menu.classList.add('active');
+    img_menu.classList.add('active');
+  }
 }
 
-function close_menu() {
-  btn_menu.classList.remove("active");
-  img_menu.classList.remove("active");
-}
-
-contato.addEventListener("click", () => {
-  dialog.style.display = "flex";
+fechar.addEventListener('click', () => {
+  main_main.style.display = 'none';
 });
 
-close.addEventListener("click", () => {
-  dialog.style.display = "none";
+contato.addEventListener('click', () => {
+  dialog.style.display = 'flex';
+});
+
+close.addEventListener('click', () => {
+  dialog.style.display = 'none';
 });
 
 document.onkeydown = function (e) {
-  if (e.key === "Escape") {
-    dialog.style.display = "none";
-    btn_menu.classList.remove("active");
-    img_menu.classList.remove("active");
+  if (e.key === 'Escape') {
+    dialog.style.display = 'none';
+    btn_menu.classList.remove('active');
+    img_menu.classList.remove('active');
   }
 };
 
 $(document).ready(function () {
-  $(".carousel").owlCarousel({
+  $('.carousel').owlCarousel({
     loop: true,
     autoplayTimeOut: 2000,
     autoplayHoverPauser: true,
